392e9bae6e19112dd500599bf3b65281082f391f
Théo Bourdier <theobourdier1@gmail.com>
2023-04-29 16:16:52 +0200

    Connect to MongoDB

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const express = require('express');
-require('dotenv').config();
-const { MongoClient } = require('mongodb');
+const express = require("express");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 const app = express();
 const appPort = 8080;
 const dbUrl = `mongodb+srv://tb:${process.env.MONGODB_PASS}@sweep-api.7o5uwy1.mongodb.net/?retryWrites=true&w=majority`;
-const dbName = 'sweep';
+const dbName = "sweep";
 
-MongoClient.connect(dbUrl, function(err, client) {
-  console.log("Connected to MongoDB");
-  const db = client.db(dbName).command({ ping: 1 });
-  client.close();
-});
+async function main() {
+  const client = new MongoClient(dbUrl);
+
+  try {
+    await client.connect();
+    await client.db(dbName).command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+main().catch(console.error);
 
 app.listen(appPort, () => {
-  console.log(`Server listening on port ${appPort}`)
-})
+  console.log(`Server listening on port ${appPort}`);
+});
