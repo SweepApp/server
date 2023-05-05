f1ad52fcdfe47cb77b166e7bbfa4065c5e04f49a
Théo Bourdier <theobourdier1@gmail.com>
2023-05-05 13:26:29 +0200

    Add temp. local CORS

--- server.mjs
+++ server.mjs
@@ -25,6 +25,12 @@ try {
 
 let db = conn.db(process.env.MONGODB);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send("<div style='text-align:center'><a href='/movies'>MOVIES</a> / <a href='/tv'>SERIES</a></div>");
 })
