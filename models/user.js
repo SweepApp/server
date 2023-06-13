63a6575fb98253a8245c6ee6154f35d49f999fd2
Théo Bourdier <theobourdier1@gmail.com>
2023-06-13 23:55:50 +0200

    Fix export

--- models/user.js
+++ models/user.js
@@ -1,23 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema(
   {
-    username: String,
     email: String,
     password: String,
+    username: String
   },
   {
     timestamps: true,
     toObject: {
       transform: (doc, ret, options) => {
-        ret.id = ret._id;
-        delete ret._id;
-        delete ret.password;
-        delete ret.__v;
-        return ret;
-      },
-    },
+        ret.id = ret._id
+        delete ret._id
+        delete ret.password
+        delete ret.__v
+        return ret
+      }
+    }
   }
-);
+)
 
-module.exports.User = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema)
