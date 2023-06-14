6ee1cdef3a1bbfd3494b0df110aafaa3d20b0914
Théo Bourdier <theobourdier1@gmail.com>
2023-06-14 14:18:35 +0200

    Add avatar field

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema(
   {
     email: String,
     password: String,
-    username: String
+    username: String,
+    avatar: String,
   },
   {
     timestamps: true,
