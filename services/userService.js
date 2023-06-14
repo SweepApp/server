5ff6312557b10cfe22e136b383760b6e7d1797c6
Théo Bourdier <theobourdier1@gmail.com>
2023-06-14 14:19:21 +0200

    Delete default secret key

--- services/userService.js
+++ services/userService.js
@@ -61,7 +61,7 @@ module.exports.loginUser = async serviceData => {
 
     const token = jwt.sign(
       { id: user._id },
-      process.env.SECRET_KEY || 'default-secret-key',
+      process.env.SECRET_KEY,
       { expiresIn: '1d' }
     );
 
